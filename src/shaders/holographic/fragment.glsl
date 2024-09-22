ad5567d665c89193a10651b1db7443e48aa3db93
XR-Tech-Space <yliang123456@outlook.com>
2024-09-22 23:24:54 +0800

    更改条纹模型
    增加条纹模型的明显度

--- src/shaders/holographic/fragment.glsl
+++ src/shaders/holographic/fragment.glsl
@@ -4,7 +4,8 @@ void main(){
 
     // Stripes
     float stripes = vPosition.y;
-    stripes = mod(vPosition.y*20.0,1.0);
+    stripes = mod(vPosition.y*15.0,1.0);
+    stripes = pow(stripes,3.0); //延缓数值陡升，让条纹渐变更明显 （0-1）
 
     // Final color
     gl_FragColor = vec4(stripes,stripes,stripes,1.0);
